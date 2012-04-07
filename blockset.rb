8883921f0c28fd2c6f15b0b8a26ba6b3154f88cf
J. Wostenberg <awosty@gmail.com>
2012-04-07 17:19:44 -0600

    Created a tokenizer

--- blockset.rb
+++ blockset.rb
@@ -3,7 +3,14 @@ PRJ_ROOT = File.expand_path(File.dirname(__FILE__))
 require "lang/evaluator"
 require "lang/bks_object"
 require "lang/lambda"
-func = BKS_NAMESPACE::Lambda.new [[{:val => BKS_NAMESPACE::Object.new}, {:val => :"printLine:", :args => [[{:val => "Hello world"}]]}]]
-Blockset::TreeEvaluator.new.eval [
-  [{:val => func}, {:val => :"call", :args => []}]
-]
+require "lang/lexer"
+#puts File.read("hello.bks")
+f = File.read("token_test")
+l = Blockset::Lexer.new(f)
+until l.end?
+  puts l.next
+end
+#func = BKS_NAMESPACE::Lambda.new [[{:val => BKS_NAMESPACE::Object.new}, {:val => :"printLine:", :args => [[{:val => "Hello world"}]]}]]
+#Blockset::TreeEvaluator.new.eval [
+#  [{:val => func}, {:val => :"call", :args => []}]
+#]
---
+++ lang/lexer.rb
@@ -0,0 +1,17 @@
+require "lexr"
+
+module Blockset
+  Lexer = Lexr.that {
+    ignores /\s+/       => :whitespace
+    ignores /--.*/      => :comment     # Comments begin with double-dashes and extend until the end of the line
+    matches "("         => :beg_paren
+    matches ")"         => :end_paren
+    matches /\d*\.\d+/  => :float,      :convert_with => lambda {|v| Float(v)}
+    matches /\d+/       => :int,        :convert_with => lambda {|v| Integer(v)}
+    matches /"[^"]*"/m  => :str
+    matches /:\w+/      => :sym,        :convert_with => lambda {|v| v.to_sym}
+    matches /\w[\w:]*/  => :m_chunk,    :convert_with => lambda {|v| v.to_sym}
+    matches "."         => :op_send_m
+    matches ";"         => :expr_end
+  }
+end
