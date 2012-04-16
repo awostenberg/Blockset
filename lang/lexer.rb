7734df2a1e6e165680145444ed4dcaec65bc2f01
J. Wostenberg <awosty@gmail.com>
2012-04-16 07:12:12 -0600

    Changed the symbol syntax from /:\w+/ to /$\w+/ (they are now preceeded with $ instead of $)

--- lang/lexer.rb
+++ lang/lexer.rb
@@ -8,8 +8,8 @@ module Blockset
     matches ")"         => :end_paren
     matches /\d*\.\d+/  => :float,      :convert_with => lambda {|v| Float(v)}
     matches /\d+/       => :int,        :convert_with => lambda {|v| Integer(v)}
-    matches /"[^"]*"/m  => :str
-    matches /:\w+/      => :sym,        :convert_with => lambda {|v| v.to_sym}
+    matches /"[^"]*"/m  => :str,        :convert_with => lambda {|v| v[1..-2]}
+    matches /\$\w+/      => :sym,       :convert_with => lambda {|v| v[1..-1].to_sym}
     matches /\w[\w:]*/  => :m_chunk,    :convert_with => lambda {|v| v.to_sym}
     matches "."         => :op_send_m
     matches ";"         => :expr_end
