089c49d5d540c095cfe3952355d92bfd769cc70d
J. Wostenberg <awosty@gmail.com>
2012-04-16 17:11:13 -0600

    Removed a few token types by actually making the token values matter for some (less verbose)

--- lang/lexer.rb
+++ lang/lexer.rb
@@ -5,14 +5,16 @@ module Blockset
   Lexer = Lexr.that {
     ignores /\s+/       => :whitespace
     ignores /--.*/      => :comment     # Comments begin with double-dashes and extend until the end of the line
-    matches "("         => :beg_paren
-    matches ")"         => :end_paren
+    matches "("         => :beg_delim
+    matches "["         => :beg_delim
+    matches ")"         => :end_delim
+    matches "]"         => :end_delim
     matches /\d*\.\d+/  => :float,      :convert_with => lambda {|v| Float(v)}
     matches /\d+/       => :int,        :convert_with => lambda {|v| Integer(v)}
     matches /"[^"]*"/m  => :str,        :convert_with => lambda {|v| v[1..-2]}
-    matches /\$\w+/      => :sym,       :convert_with => lambda {|v| v[1..-1].to_sym}
+    matches /\$\w+/     => :sym,       :convert_with => lambda {|v| v[1..-1].to_sym}
     matches /\w[\w:]*/  => :m_chunk,    :convert_with => lambda {|v| v.to_sym}
-    matches "."         => :op_send_m
-    matches ";"         => :expr_end
+    matches "."         => :op
+    matches ";"         => :op
   }
 end
